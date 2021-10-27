a6a3c5b020610cd89d1bcad6c6115ef8d667f456
Sytailo Viacheslav <sigurdson.workspace@gmail.com>
2021-10-27 21:44:09 +0300

    add Homework2

---
+++ Homework2/Homework2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Homework2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int height = scan.nextInt();
+        int count = scan.nextInt();
+        System.out.println("The height building " + height + " m and " + count + " floors" );
+    }
+}
