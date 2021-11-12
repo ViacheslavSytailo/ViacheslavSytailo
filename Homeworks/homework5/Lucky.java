07d17b843fe4e20dcaceee2e7d133fd28f669a64
Sytailo Viacheslav <sigurdson.workspace@gmail.com>
2021-11-12 14:11:35 +0200

    add Homework5

---
+++ Homeworks/homework5/Lucky.java
@@ -0,0 +1,31 @@
+package homework5;
+import java.util.Random;
+
+
+public class Lucky {
+
+
+    public static void checkLucky(String[] args)
+    {
+        int[] array1 = {15, 8, 24, 41, 9, 84};
+        Random random = new Random();
+        int index = random.nextInt(array1.length);
+        int indexRandom = array1[index];
+        if ((indexRandom % 2) == 0) {
+            System.out.println("I am lucky");
+        } else {
+            System.out.println("Run again");
+        }
+    }
+
+    public static boolean checkLuckyByInt(int number)
+    {
+        if ((number % 2) == 0) {
+            System.out.println("I am lucky");
+            return true;
+        } else {
+            System.out.println("Run again");
+            return false;
+        }
+    }
+}
---
+++ test/LuckyTest.java
@@ -0,0 +1,67 @@
+import homework5.Lucky;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class LuckyTest {
+    Lucky lucky = new Lucky();
+
+    @DataProvider(name="correctArrayData")
+    public Object[][] correct()
+    {
+        return new Object[][] {
+                {8},
+                {24},
+                {84}
+        };
+    }
+
+    @DataProvider(name="incorrectArrayData")
+    public Object[][] incorrect()
+    {
+        return new Object[][] {
+                {15},
+                {41},
+                {9}
+        };
+    }
+
+@Test(dataProvider = "correctArrayData")
+    public static void testCheckLucky(int number)
+{
+
+    Assert.assertEquals((number % 2), 0, "Odd number");
+}
+
+    @Test(dataProvider = "incorrectArrayData")
+    public static void testCheckLucky1(int number)
+    {
+
+        Assert.assertEquals((number % 2), 1, "Odd number");
+    }
+
+
+@BeforeTest
+    public void beforeTest()
+{
+    System.out.println("This is text before each test");
+}
+
+    @BeforeSuite
+    public void beforeSuit() {
+        System.out.println("Before all tests ….");
+    }
+
+    @AfterTest
+    public void afterTest() {
+        System.out.println("This is text after each test");
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        System.out.println("After all tests ….");
+    }
+}
+
+
+
+
